49c72fe5c5830c8b394112637d47cd29fbc2bea7
Ibrahim  Akar <ibsakar@gmail.com>
2019-09-25 15:13:42 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,6 +1,6 @@
-def turn_count(turn)
+def turn_count(board)
     counter = 0
-    turn.each do |index|
+    board.each do |index|
       if index == "X" || index == "O"
         counter = counter + 1
     end 
