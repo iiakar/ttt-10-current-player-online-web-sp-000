16460777bc6ede240d1ae0274ec86a3334d37e9e
Ibrahim  Akar <ibsakar@gmail.com>
2019-09-25 14:54:07 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,23 +9,23 @@ def turn_count(turn)
 end
 
 
-def current_player(counter)
-if counter == 0 || 2 || 4 || 6 || 8
-  return "X" 
-elsif  
-  counter == 1 || 3 || 5 || 7 || 9
-    return "O"
-  
-  end 
-
-end 
-
-
 # def current_player(counter)
-# if counter == 0
+# if counter == 0 || 2 || 4 || 6 || 8
 #   return "X" 
-#   counter.even?
+# elsif  
+#   counter == 1 || 3 || 5 || 7 || 9
 #     return "O"
-#   else 
-#     return "X"
+  
 #   end 
+
+# end 
+
+
+def current_player(counter)
+if counter == 0
+  return "X" 
+  counter.even?
+    return "O"
+  else 
+    return "X"
+  end 
